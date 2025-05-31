d0be00c9e79ae0780c51b7ef4a8b1e8ed5b57caf
ducchi1109 <ducchi1109@gmail.com>
2025-05-31 13:47:41 +0700

    Delete nvim directory

--- nvim/lua/configs/mason-dap.lua
+++
@@ -1,8 +0,0 @@
-require("mason-nvim-dap").setup({
-    ensure_installed = { "python", "codelldb" },
-    handlers = {
-        function(config)
-            require("mason-nvim-dap").default_setup(config)
-        end,
-    },
-})
